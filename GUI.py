e85984944c09d702c33df11f4dd7e63cf6a70631
kristin <kristin.huseby@inmeta.no>
2019-10-01 13:56:30 +0200

    new checkbox

--- GUI.py
+++ GUI.py
@@ -12,5 +12,11 @@ combo = Combobox(window)
 combo['values']= (1, 2, 3, 4, 5, 'Text')
 combo.current(1) #set the selected item
 combo.grid(column=0, row=0)
+combo.get()
+
+chk_state = BooleanVar()
+chk_state.set(True) #set check state
+chk = Checkbutton(window, text='Choose', var=chk_state)
+chk.grid(column=0, row=0)
 
 window.mainloop()
