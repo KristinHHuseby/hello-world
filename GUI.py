a1d9a878b7bd11e714e2a36e0a51d8c8d88c9a41
kristin <kristin.huseby@inmeta.no>
2019-10-01 14:21:29 +0200

    clicka clicka

--- GUI.py
+++ GUI.py
@@ -2,23 +2,27 @@ from tkinter import *
 from tkinter.ttk import *
 
 
+def clicked():
+    print(selected.get())
 
 
 window = Tk()
 window.title("Welcome to LikeGeeks app")
 window.geometry('400x200')
 
-combo = Combobox(window)
-combo['values']= (1, 2, 3, 4, 5, 'Text')
-combo.current(1) #set the selected item
-combo.grid(column=0, row=0)
-combo.get()
 
-chk_state = BooleanVar()
-chk_state.set(True) #set check state
-chk = Checkbutton(window, text='Choose', var=chk_state)
-chk.grid(column=0, row=0)
+btn = Button(window, text='click me', command=clicked)
 
-rad1 = Radiobutton(window, text='First', value=1)
-rad1.grid(column=1, row=3)
+
+
+selected = IntVar()
+rad1 = Radiobutton(window, text='First', value=1, variable=selected)
+rad2 = Radiobutton(window, text='Second', value=2, variable=selected)
+rad3 = Radiobutton(window, text='99', value=3, variable=selected)
+rad1.grid(column=1, row=1)
+rad2.grid(column=2, row=2)
+rad3.grid(column=2, row=3)
+btn.grid(column=1, row=0)
+
+
 window.mainloop()
